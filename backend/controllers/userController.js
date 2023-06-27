b80abfe8054ad1380910a4c3956abbda57bc8450
unknown <abdullah@pace-tech.com>
2023-06-27 17:02:32 +0500

    backend complete

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -63,6 +63,24 @@ const profile = asyncHandler(async (req, res) => {
   res.status(200).json(user);
 });
 const updateProfile = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user._id);
+  if (user) {
+    user.name = req.body.name || user.name;
+    user.email = req.body.email || user.email;
+
+    if (req.body.password) {
+      user.password = req.body.password;
+    }
+    const updatedUser = await user.save();
+    res.status(200).json({
+      _id: updatedUser._id,
+      name: updatedUser.name,
+      email: updatedUser.email,
+    });
+  } else {
+    res.status(400);
+    throw new Error(`User Not Found`);
+  }
   res.status(200).json({ message: ' Update Profile' });
 });
 
